40f6cbacc563635fcecf7df91d0f340c1467e761
moustafa011494 <127332000+moustafa011494@users.noreply.github.com>
2024-10-23 02:12:37 +0300

    Update طرد.js

--- plugins/طرد.js
+++ plugins/طرد.js
@@ -7,7 +7,7 @@ if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`*تـــم طردك من النقابة بواسطة فلاش بوت تعالي بوس حذائي عشان ارجعك !*`) 
+m.reply(`*تـــم طردك من النقابة بواسطة البوت تعالي بوس حذائي عشان ارجعك !*`) 
 }
 
 handler.help = ['kick @user']
