c55227b292c61afb2ec471b170c72d0f95db226c
moustafa011494 <127332000+moustafa011494@users.noreply.github.com>
2024-10-23 02:02:44 +0300

    Update م1(1).js

--- plugins/م1(1).js
+++ plugins/م1(1).js
@@ -25,11 +25,11 @@ let more = String.fromCharCode(8206)
 let readMore = more.repeat(850)   
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let str = `⎔⋅• ━ ╼╃ ⌬〔﷽〕⌬ ╄╾ ━ •⋅⎔
-> TheFlash-Bot-MD 
+> MOSHIREF﹝⚡﹞BOT 
 *⎔⋅ ╼╃ ⊰ •﹝⚡﹞• ⊱ ╄╾ ⋅⎔*
 > 〔قـسـم الاعـضـاء┊ ˼‏ 🎸˹ 〕
 *⎔⋅ ╼╃ ⊰ •﹝⚡﹞• ⊱ ╄╾ ⋅⎔*
-           *فلاش بــ🤖ــوت*
+           * بــ🤖ــوت*
 *⎔⋅ ╼╃ ⊰ •﹝⚡﹞• ⊱ ╄╾ ⋅⎔*
 > 🎸.عمل
 > 🎸.طرد
@@ -53,7 +53,7 @@ let str = `⎔⋅• ━ ╼╃ ⌬〔﷽〕⌬ ╄╾ ━ •⋅⎔
 > 🎸.ترتيب
 *⎔⋅ ╼╃ ⊰ •﹝⚡﹞• ⊱ ╄╾ ⋅⎔*
 *~⌬ تــ✍️ــوقــيــع ↡~*🖋
-*TheFlash-Bot-MD*
+*MOUSTAFA SHIREF*
 *⎔⋅ ╼╃ ⊰ •﹝⚡﹞• ⊱ ╄╾ ⋅⎔*`.trim()
    const _0x3c7cab = _0x5a1d;
 
